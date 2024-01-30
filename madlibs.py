50fc3d2dfeb2a0df41921934cd3062fc210e701a
PineappleOnPizza12345 <157723383+PineappleOnPizza12345@users.noreply.github.com>
2024-01-30 15:06:58 +0000

    done

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,43 @@
+words = []
 
+answer = input("Give me an adjective")
+
+words.append(answer)
+
+answer = input("Give me a store")
+
+words.append(answer)
+
+answer = input("Give me a noun.")
+
+words.append(answer)
+
+answer = input("Give me another noun")
+
+words.append(answer)
+
+answer = input("Give me an adverb")
+
+words.append(answer)
+
+answer = input("Give me a verb")
+
+words.append(answer)
+
+answer = input("Give me another verb")
+
+words.append(answer)
+
+answer = input("Give me a place")
+
+words.append(answer)
+
+answer = input("Give me a noun")
+
+words.append(answer)
+
+answer = input("Give me an adjective ending in ed")
+
+words.append(answer)
+
+print(f"A {words[0]} person went to {words[1]} to get a {words[2]}. They liked the {words[3]} and went to the shop to {words[4]} {words[5]}. Then they, {words[6]} to {words[7]} for a {words[8]}. They {words[9]} it.")
